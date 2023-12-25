e437e1afb03a0d8bf883ee2b3e8480dd0cb3a581
Vaibhav Kumar Singh <117349059+vaibhavkumarsingh2112@users.noreply.github.com>
2023-12-25 11:56:07 +0530

    Create NumberGuessingGame.java
    Task 1 Number Guessing Game

---
+++ NumberGuessingGame.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Main {
+
+  static int randomNumber() {
+
+    return (int) (Math.random() * 100) + 1;
+  }
+
+  public static void main(String[] args) {
+    int random_number = randomNumber();
+    int n;
+    int attempts = 5;
+
+    System.out.println("    NUMBER GUESSING GAME");
+    System.out.println("\n");
+    System.out.println("  You have 5 attempts to guess the number");
+
+    System.out.print("  Enter a Number (1-100) : ");
+
+    Scanner scanner = new Scanner(System.in);
+
+    n = scanner.nextInt();
+
+    while (n != random_number && attempts > 0) {
+      if (n > random_number) {
+        System.out.println("  Too High");
+
+      } else if (n < random_number) {
+        System.out.println("  Too Low");
+
+      }
+      attempts--;
+      System.out.print("  Enter a Number (1-100) : ");
+      n = scanner.nextInt();
+
+    }
+    if (n == random_number) {
+      System.out.println("You Won! Dare to play again?");
+
+    } else {
+      System.out.println("  Sorry, you've run out of attempts. The correct number was: " + random_number);
+    }
+    System.out.println();
+    System.out.print("  Dare to play again? (y or n) ");
+    String playAgain = scanner.next();
+    System.out.println();
+    if (playAgain.equals("y")) {
+      main(args);
+
+    } else {
+
+    }
+
+  }
+}
